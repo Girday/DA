49c47f4a004899dc4297aaad69966fa8741e0071
Maxim Grigorev <maxkraft06@gmail.com>
2026-02-19 00:29:02 +0300

    add key-value input

--- lab1_var1-1/main.cpp
+++ lab1_var1-1/main.cpp
@@ -9,5 +9,11 @@ using Object = std::pair<Key, Value>;
 
 int main() {
     std::vector<Object> objects;
+    
+    Key key;
+    Value value;
+    while (std::cin >> key >> value)
+        objects.push_back({key, value});
+
     return 0;
 }
