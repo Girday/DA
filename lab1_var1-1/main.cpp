9bc5d517491697fa9a09d1d7305db247dba34f0e
Maxim Grigorev <maxkraft06@gmail.com>
2026-02-19 00:04:11 +0300

    add Key, Value & Object

--- lab1_var1-1/main.cpp
+++ lab1_var1-1/main.cpp
@@ -3,7 +3,11 @@
 #include <utility>
 #include <string>
 
+using Key = int;
+using Value = std::string;
+using Object = std::pair<Key, Value>;
+
 int main() {
-    std::vector<std::pair<int, std::string>> objects;
+    std::vector<Object> objects;
     return 0;
 }
