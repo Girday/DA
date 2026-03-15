54e85ff83b8dbc69321975485108d7768570cd63
Maxim Grigorev <maxkraft06@gmail.com>
2026-03-15 13:04:29 +0300

    change 0 to 1 and -1 to 0 for returns

--- my_tries/trie/trie.cpp
+++ my_tries/trie/trie.cpp
@@ -18,22 +18,22 @@ bool IsEmpty(Trie trie) {
 
 int Search(Trie trie, std::string word) {
     if (IsEmpty(trie)) // Если trie пустой, то там нет слов
-        return -1;
+        return 0;
         
     auto& children = trie->children; // Берём словарик детей
 
     if (word.size() == 0) { // Если искомое слово закончилось, проверяем, есть ли в конце sentinel ($)
         if (auto iter = children.find('$'); iter != children.end())
-            return 0; // Если есть, то слово нашлось
+            return 1; // Если есть, то слово нашлось
         else
-            return -1; // Если нет, то слово не нашлось
+            return 0; // Если нет, то слово не нашлось
     }
 
     auto letter = word[0]; // Берём первую букву искомого слова
     if (auto iter = children.find(letter); iter != children.end()) // Ищем эту букву в словарике детей
         Search(iter->second, word.substr(1)); // Если такая нашлась, то запускаем поиск подслова после этой буквы
     else
-        return -1; // Если нет, то слово не нашлось
+        return 0; // Если нет, то слово не нашлось
 }
 
 void DestroyTrie(Trie trie) {
